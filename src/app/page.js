e08144acf96a6f5bbb205aa45089fbc6fb4f23f1
chen.lingzhi <chen.lingzhi@qkupower.com>
2024-09-19 19:38:05 +0800

    修改

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./page.css";
 import axios from "@/app/lib/axios";
+import { Button, message, Spin } from "antd";
 const ImageUploadAndEdit = () => {
   const [params, setParams] = useState({});
   const [images, setImages] = useState({
@@ -13,6 +14,13 @@ const ImageUploadAndEdit = () => {
     imageUrl2: null,
     imageUrl3: null,
     imageUrl4: null,
+  });
+  const [loading, setLoading] = useState({
+    img1: false,
+    img2: false,
+    img3: false,
+    img4: false,
+    loadingImg5: false,
   });
   const [taskID, setTaskID] = useState();
   const canvasRefs = {
@@ -32,7 +40,10 @@ const ImageUploadAndEdit = () => {
   const handleUpload = async (event, imgKey, urlKey) => {
     const file = event.target.files[0];
     if (!file) return;
-
+    setLoading((pre) => ({
+      ...pre,
+      [imgKey]: true,
+    }));
     try {
       const response = await fetch(`/api/dashboard?filename=${file.name}`, {
         method: "POST",
@@ -90,7 +101,6 @@ const ImageUploadAndEdit = () => {
       img.src = images.imageUrl4;
       console.log(img.src);
       img.crossOrigin = "Anonymous"; // Ensure cross-origin compatibility if needed
-      console.log(img.naturalWidth, "img.naturalWidth");
       img.onload = () => {
         const canvas = canvasRefs["canvas6"].current;
         const ctx = canvas.getContext("2d");
@@ -111,6 +121,24 @@ const ImageUploadAndEdit = () => {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img, 0, 0, img.width, img.height);
+    if (canvasKey === "canvas1") {
+      setLoading((pre) => ({
+        ...pre,
+        img1: false,
+      }));
+    }
+    if (canvasKey === "canvas3") {
+      setLoading((pre) => ({
+        ...pre,
+        img2: false,
+      }));
+    }
+    if (canvasKey === "canvas5") {
+      setLoading((pre) => ({
+        ...pre,
+        img3: false,
+      }));
+    }
   };
 
   const handleMouseDown = (paintingKey) => {
@@ -251,24 +279,114 @@ const ImageUploadAndEdit = () => {
   };
 
   const getImg2 = async () => {
-    const result = await axios.post("/api/proxy", {
-      path: "img2img/query",
-      taskID: taskID,
-    });
-    if (result?.data?.data?.progress === 100) {
-      setImages((prevImages) => ({
-        ...prevImages,
-        imageUrl4: result.data.data.imageFiles[0].url,
-      }));
-    }
-    console.log("progress", result?.data?.data?.progress);
+    let attemptCount = 0; // 计数器，用于记录轮询次数
+
+    const checkProgress = async () => {
+      attemptCount++; // 每次轮询时增加计数
+
+      try {
+        const result = await axios.post("/api/proxy", {
+          path: "img2img/query",
+          taskID: taskID,
+        });
+        const progress = result?.data?.data?.progress;
+        console.log("progress", progress);
+
+        // 如果进度是 100，停止轮询
+        if (progress === 100) {
+          setImages((prevImages) => ({
+            ...prevImages,
+            imageUrl4: result.data.data.imageFiles[0].url,
+          }));
+          setLoading((pre) => ({
+            ...pre,
+            img4: false,
+          }));
+          return;
+        }
+
+        // 如果轮询次数达到5次，停止轮询
+        if (attemptCount >= 6) {
+          console.log("Reached max attempts, stopping polling");
+          setLoading((pre) => ({
+            ...pre,
+            img4: false,
+          }));
+          message.info("生成图片错误，请重试！");
+          return;
+        }
+
+        const delay = progress < 50 ? 30000 : 15000;
+        setTimeout(checkProgress, delay);
+      } catch (error) {
+        console.error("Error fetching progress:", error);
+        // 如果发生错误，也停止轮询
+        setLoading((pre) => ({
+          ...pre,
+          img4: false,
+        }));
+        message.info("生成图片错误，请重试！");
+      }
+    };
+
+    // 开始首次检查进度
+    checkProgress();
   };
+
+  const handleTouchStart = (paintingKey) => {
+    setPainting((prevState) => ({
+      ...prevState,
+      [paintingKey]: true,
+    }));
+  };
+
+  const handleTouchEnd = (paintingKey) => {
+    setPainting((prevState) => ({
+      ...prevState,
+      [paintingKey]: false,
+    }));
+    const canvasKey = paintingKey === "painting1" ? "canvas1" : "canvas3";
+    const ctx = canvasRefs[canvasKey].current.getContext("2d");
+    ctx.beginPath();
+  };
+
+  const handleTouchMove = (e, paintingKey) => {
+    if (!painting[paintingKey]) return;
+
+    const canvasKey = paintingKey === "painting1" ? "canvas1" : "canvas3";
+    const canvas = canvasRefs[canvasKey].current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+
+    // 处理触摸事件的位置
+    const touch = e.touches[0]; // 获取第一根手指
+    const x = (touch.clientX - rect.left) * scaleX;
+    const y = (touch.clientY - rect.top) * scaleY;
+
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "rgba(255, 0, 0, 0.7)";
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  };
+
   useEffect(() => {
     if (Object.keys(params).length === 5) {
       getImg();
     }
   }, [params]);
-
+  useEffect(() => {
+    if (taskID) {
+      getImg2();
+    }
+  }, [taskID]);
   return (
     <div className="container">
       <div className="upload-section">
@@ -279,18 +397,25 @@ const ImageUploadAndEdit = () => {
           onChange={(e) => handleUpload(e, "img1", "imageUrl1")}
           className="upload-input"
         />
-        {images.imageUrl1 && (
-          <div className="canvas-container">
-            <canvas
-              ref={canvasRefs.canvas1}
-              onMouseDown={() => handleMouseDown("painting1")}
-              onMouseUp={() => handleMouseUp("painting1")}
-              onMouseMove={(e) => handleMouseMove(e, "painting1")}
-              className="canvas"
-            />
-            <canvas ref={canvasRefs.canvas2} style={{ display: "none" }} />
+        <Spin spinning={loading.img1} delay={500}>
+          <div className="max-h">
+            {images.imageUrl1 && (
+              <div className="canvas-container">
+                <canvas
+                  ref={canvasRefs.canvas1}
+                  onMouseDown={() => handleMouseDown("painting1")}
+                  onMouseUp={() => handleMouseUp("painting1")}
+                  onMouseMove={(e) => handleMouseMove(e, "painting1")}
+                  onTouchStart={() => handleTouchStart("painting1")}
+                  onTouchEnd={() => handleTouchEnd("painting1")}
+                  onTouchMove={(e) => handleTouchMove(e, "painting1")}
+                  className="canvas"
+                />
+                <canvas ref={canvasRefs.canvas2} style={{ display: "none" }} />
+              </div>
+            )}
           </div>
-        )}
+        </Spin>
       </div>
       <div className="upload-section">
         <h3>上传风格图</h3>
@@ -300,18 +425,25 @@ const ImageUploadAndEdit = () => {
           onChange={(e) => handleUpload(e, "img2", "imageUrl2")}
           className="upload-input"
         />
-        {images.imageUrl2 && (
-          <div className="canvas-container">
-            <canvas
-              ref={canvasRefs.canvas3}
-              onMouseDown={() => handleMouseDown("painting2")}
-              onMouseUp={() => handleMouseUp("painting2")}
-              onMouseMove={(e) => handleMouseMove(e, "painting2")}
-              className="canvas"
-            />
-            <canvas ref={canvasRefs.canvas4} style={{ display: "none" }} />
+        <Spin spinning={loading.img2} delay={500}>
+          <div className="max-h">
+            {images.imageUrl2 && (
+              <div className="canvas-container">
+                <canvas
+                  ref={canvasRefs.canvas3}
+                  onMouseDown={() => handleMouseDown("painting2")}
+                  onMouseUp={() => handleMouseUp("painting2")}
+                  onMouseMove={(e) => handleMouseMove(e, "painting2")}
+                  onTouchStart={() => handleTouchStart("painting2")}
+                  onTouchEnd={() => handleTouchEnd("painting2")}
+                  onTouchMove={(e) => handleTouchMove(e, "painting2")}
+                  className="canvas"
+                />
+                <canvas ref={canvasRefs.canvas4} style={{ display: "none" }} />
+              </div>
+            )}
           </div>
-        )}
+        </Spin>
       </div>
       <div className="upload-section">
         <h3>上传LOGO</h3>
@@ -321,36 +453,49 @@ const ImageUploadAndEdit = () => {
           onChange={(e) => handleUpload(e, "img3", "imageUrl3")}
           className="upload-input"
         />
-        {images.imageUrl3 && (
-          <div className="canvas-container">
-            <canvas ref={canvasRefs.canvas5} className="canvas" />
+        <Spin spinning={loading.img3} delay={500}>
+          <div className="max-h">
+            {images.imageUrl3 && (
+              <div className="canvas-container">
+                <canvas ref={canvasRefs.canvas5} className="canvas" />
+              </div>
+            )}
           </div>
-        )}
+        </Spin>
       </div>
-      <div className="upload-section">
-        {images.imageUrl4 && (
-          <div className="canvas-container">
-            <canvas ref={canvasRefs.canvas6} className="canvas" />
-          </div>
-        )}
-      </div>
-      <button
-        className="submit-button"
-        onClick={() => {
-          handleSubmit("canvas1", "canvas2");
-          handleSubmit("canvas3", "canvas4");
-        }}
-      >
-        提交图片
-      </button>
-      <button
+      <Spin spinning={loading.img4} delay={500}>
+        <div className="upload-section">
+          {images.imageUrl4 && (
+            <div className="canvas-container">
+              <canvas ref={canvasRefs.canvas6} className="canvas" />
+            </div>
+          )}
+        </div>
+        <Button
+          type="primary"
+          className="submit-button"
+          onClick={() => {
+            setLoading((pre) => ({
+              ...pre,
+              img4: true,
+            }));
+            message.info("正在生成图片");
+            handleSubmit("canvas1", "canvas2");
+            handleSubmit("canvas3", "canvas4");
+          }}
+        >
+          提交图片
+        </Button>
+      </Spin>
+      {/* <Button
+        type="primary"
         className="submit-button"
         onClick={() => {
           getImg2();
         }}
       >
         获取图片
-      </button>
+      </Button> */}
     </div>
   );
 };
