a53a5ff83a287697c873b9dfc6884de48c73494d
lz <13714733197@163.com>
2024-09-24 17:51:11 +0800

    修改

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,7 @@ const nextConfig = {
     images: {
         domains: ['mind-file.oss-cn-beijing.aliyuncs.com','chimerai-file.oss-cn-beijing.aliyuncs.com'], 
       },
-      // i18n: {
-      //   locales: ['en', 'zh'], 
-      //   defaultLocale: 'en', 
-      //   localeDetection: true, 
-      // },
+
 };
 
 export default nextConfig;
